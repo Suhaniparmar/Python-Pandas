c19682cb65a5019536a506dedda713e4b53677bb
Suhaniparmar <1008suhaniparmar@gmail.com>
2024-05-21 00:26:00 +0530

    complete_3rd

--- Pandas/3_subset_of_df.py
+++ Pandas/3_subset_of_df.py
@@ -16,3 +16,25 @@ follower = file3[file3["Interaction"].isin([6,9])]
 # or
 #follower =  file3[(file3["Followers"]==20) | (file3["Followers"]==21) | (file3["Followers"]==22)]
 print(follower)
+
+no_NaN = file3[file3["Interaction"].notna()]
+print("\n Not NaN interactions are here\n")
+print(no_NaN)
+print(no_NaN.shape)
+
+high_reach = file3.loc[file3["Reach"]>5000,"Date"]
+print(high_reach)
+print(high_reach.shape)
+
+print(file3.iloc[5:10, 2:5])
+
+# REMEMBER :
+# When selecting subsets of data, square brackets [] are used.
+
+# Inside these brackets, you can use a single column/row label, a list of column/row labels, a slice of labels, a conditional expression or a colon.
+
+# Select specific rows and/or columns using loc when using the row and column names.
+
+# Select specific rows and/or columns using iloc when using the positions in the table.
+
+# You can assign new values to a selection based on loc/iloc.
